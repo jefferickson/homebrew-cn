6a93d7887a7b252137fe484239652fa463652948
Jeff Erickson <jeff@erick.so>
2019-03-16 13:36:03 -0400

    release v0.3.0

--- cn.rb
+++ cn.rb
@@ -1,8 +1,8 @@
 class Cn < Formula
-    desc "Find the index of a label in a CSV header"
+    desc "Find the index of a label in a CSV header, or print a column of data from a CSV based on the header label."
     homepage "https://github.com/jefferickson/cn"
-    url "https://github.com/jefferickson/cn/archive/0.2.0.tar.gz"
-    sha256 "b54addb658d675b13f3f43a3d657f4c9a663fac22c32f83d5e494a282b517dc4"
+    url "https://github.com/jefferickson/cn/archive/0.3.0.tar.gz"
+    sha256 "eba966d00f64afd7895607c74a5772d6e3377cf6538f8652343f7e2efe31c6f7"
     head "https://github.com/jefferickson/cn.git"
 
     depends_on "go" => :build
